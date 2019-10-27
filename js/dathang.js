a12a3b08020a24ea2187f0105c7c9bdb1e8629aa
proxyht <caohoangtung2001@gmail.com>
2019-10-27 16:04:04 +0700

    first commit

---
+++ js/dathang.js
@@ -0,0 +1,18 @@
+document.getElementById('search').onkeyup = function(){
+    let searchText = document.getElementById('search').value;
+    let elems = document.getElementsByClassName("champimg");
+    if (searchText != ''){
+        for (elem of elems){
+            console.log(elem.classList.value.includes(searchText),searchText);
+            if (!elem.classList.value.includes(searchText)){
+                elem.classList.remove("show");
+                elem.classList.add("hide");
+            }
+        }
+    } else {
+        for (elem of elems){
+            elem.classList.remove("hide");
+            elem.classList.add("show");
+        }
+    }
+}
